9e23a52423c8a90e6c3f0fb430a221d34fec6f25
Krit <kritdevelop@gmail.com>
2021-04-20 20:10:03 +0700

    app-client - call app-server api

--- app-client/src/app/data.service.ts
+++ app-client/src/app/data.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { throwError } from 'rxjs'; 
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  retrieveData() {
+    return this.httpClient
+      .get(`http://localhost:8080/api/message`)
+      .pipe( 
+        catchError(error => {  return throwError('retrieveData Error.'); }) 
+      )
+  }
 }
