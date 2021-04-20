3623caab314f22f7ea4f262292edfe136d512169
Krit <kritdevelop@gmail.com>
2021-04-20 20:17:19 +0700

    app-client - Configuring application environments

--- app-client/src/app/data.service.ts
+++ app-client/src/app/data.service.ts
@@ -2,17 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { throwError } from 'rxjs'; 
 import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  //consume environment setting
+  apiUrl = environment.apiUrl;
+
   constructor(private httpClient: HttpClient) { }
 
   retrieveData() {
     return this.httpClient
-      .get(`http://localhost:8080/api/message`)
+      .get(`${this.apiUrl}/api/message`)
       .pipe( 
         catchError(error => {  return throwError('retrieveData Error.'); }) 
       )
