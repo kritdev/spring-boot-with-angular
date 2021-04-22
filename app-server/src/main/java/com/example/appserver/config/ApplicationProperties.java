39691d3257c97a17d6afa32c8bf1603495ccb779
Krit <kritdevelop@gmail.com>
2021-04-23 06:51:21 +0700

    Create User and Authority Entities (MySQL)

---
+++ app-server/src/main/java/com/example/appserver/config/ApplicationProperties.java
@@ -0,0 +1,9 @@
+package com.example.appserver.config;
+
+public class ApplicationProperties {
+
+  // Regex for acceptable logins
+  public static final String LOGIN_REGEX =
+      "^(?>[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*)|(?>[_.@A-Za-z0-9-]+)$";
+
+}
