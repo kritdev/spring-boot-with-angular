f3ed71897c238df2c7866887a54979e8902a5612
Krit <kritdevelop@gmail.com>
2021-04-26 16:34:49 +0700

    app-client - update app component

--- app-client/src/app/app.component.ts
+++ app-client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { DataService } from './data.service';
+import { AccountService } from './security/auth/account.service';
+import { LoginService } from './security/login/login.service';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +10,48 @@ import { DataService } from './data.service';
 })
 export class AppComponent {
   data = null;
+  currentLogin = null;
 
-  constructor(private dataService: DataService) {
+  constructor(private dataService: DataService, 
+    private accountService: AccountService,
+    private loginService:LoginService) {
   }
 
   ngOnInit(): void {
+    // try to log in automatically
+    this.accountService.identity().subscribe();
+    
+    this.getData();
+  }
+
+  getData() {
+    this.data = null;
+    
     this.dataService
       .retrieveData()
       .subscribe(
         result => { this.data = result as any[]; },
         err => { alert(err); }
       );
+  }
 
+  isAuthenticated(): boolean {
+    if (this.accountService.isAuthenticated()) {
+      this.getLoginName();
+      return true;
+    } 
+
+    return false;
+  }
+
+  getLoginName() {
+    this.accountService.identity()
+      .subscribe(
+        account => { this.currentLogin = account.login; }
+      )
+  }
+
+  logout() {
+    this.loginService.logout();
   }
 }
--- app-client/src/app/app.module.ts
+++ app-client/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxWebstorageModule } from 'ngx-webstorage';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './security/login/login.component';
+import { AuthInterceptor } from './security/interceptor/auth.interceptor';
+import { AuthExpiredInterceptor } from './security/interceptor/auth-expired.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule, FormsModule, ReactiveFormsModule,
+    NgxWebstorageModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthExpiredInterceptor,
+      multi: true,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
