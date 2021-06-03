52f0bcfe8c0f51d10f1ae4cc8dfa84563a78cf4c
Krit <kritdevelop@gmail.com>
2021-06-03 08:32:40 +0700

    app-server - fill report with JRBeanCollectionDataSource instead of jdbc

--- app-server/src/main/java/com/example/appserver/rest/ReportResource.java
+++ app-server/src/main/java/com/example/appserver/rest/ReportResource.java
@@ -3,30 +3,32 @@ package com.example.appserver.rest;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.appserver.repository.DataMessageRepository;
+import com.example.appserver.rest.model.DataMessage;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @RestController
 @RequestMapping("/api")
 public class ReportResource {
 
   @Autowired
-  JdbcTemplate jdbcTemplate;
+  DataMessageRepository dataMessageRepository;
 
   @GetMapping("/report/pdf")
   public byte[] generateReportpdf(HttpServletResponse response)
@@ -35,16 +37,17 @@ public class ReportResource {
     response.setHeader("Content-Disposition", String.format("attachment;filename=\"sample.pdf\""));
     ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-    Connection connection = jdbcTemplate.getDataSource().getConnection();
     File file = ResourceUtils.getFile("classpath:reports/sample_report.jrxml");
     JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
 
+    List<DataMessage> dataList = dataMessageRepository.findAll();
     Map<String, Object> parameters = new HashMap<>();
     // parameters.put("id", clientID);
-    JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, connection);
+    JRBeanCollectionDataSource ds = new JRBeanCollectionDataSource(dataList);
+
+    JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, ds);
     JasperExportManager.exportReportToPdfStream(jasperPrint, out);
 
-    connection.close();
     return out.toByteArray();
   }
 }
